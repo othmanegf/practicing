ee1628daef1a7b84b649ba4037fac9849fac801d
othmanegf <147095181+othmanegf@users.noreply.github.com>
2023-10-18 23:27:59 +0100

    Add files via upload

---
+++ password.py
@@ -0,0 +1,11 @@
+
+for i in range( 0,3) :  
+    mot=input("Type password:")
+    if mot=="DEV@2023" :
+        print("Hello")
+if mot!="DEV@2023" :
+    j=input("Type the name of your favorite film:")
+    if j=="Titanic" :
+        print("Hello")
+    else :
+     print("Access denied")
